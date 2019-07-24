ca373b1c55b338e10ac180b7713fd0760b3d4a6a
soflabs <ziri.sofiane@gmail.com>
2019-07-24 11:30:13 +0200

    signupscreen

--- Components/Screens/signUpScreen.js
+++ Components/Screens/signUpScreen.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Input, Button } from "react-native-elements";
 
 class SignUpScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+         
+        <Input placeholder="FirstName" />
 
-    render() {
-  
-        return (
-  
-          <View >
-           
-            <Text>COUCOU</Text>
+        <Input placeholder="LastName" />
+        <Input placeholder="Email" />
+        <Input placeholder="Password" />
+        <Input placeholder="City" value="" />
+        <Button onPress={() => {}} title="Signup" />
+      </View>
+    );
+  }
 
-          </View>
-  
-        );
-    }
+
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#d6d7da',
+  },
+  Button: {
+    backgroundColor: '#dfe4ea',
 
-
-
-
+    fontSize: 2,
+   
+  }
+});
 export default SignUpScreen;
