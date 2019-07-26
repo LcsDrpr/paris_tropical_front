6ba2debc88db4e5cbf3b5dccc0828a8da5806f38
soflabs <ziri.sofiane@gmail.com>
2019-07-26 17:01:35 +0200

    page map + picker + listitem pour le choix du client

--- Components/Screens/mapScreen.js
+++ Components/Screens/mapScreen.js
@@ -5,7 +5,31 @@ import { Marker } from "react-native-maps";
 import * as Permissions from "expo-permissions";
 import * as Location from "expo-location";
 import { Icon, Container, Header, Content, Right } from 'native-base';
+import { ListItem } from 'react-native-elements';
 
+
+
+
+class Menupicker extends Component {
+  state = {
+    language: null
+  }
+  render() {
+    return (
+        <Picker 
+          selectedValue={this.state.language}
+          style={{ height: 50, width: 100,  }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ language: itemValue })
+          }
+        >
+          <Picker.Item label="Java" value="java" />
+          <Picker.Item label="JavaScript" value="js" />
+        </Picker>
+     
+    );
+  }
+}
 
 class MapScreen extends Component {
   state = {
@@ -14,7 +38,6 @@ class MapScreen extends Component {
     logPosition: [],
 
     displayHistorique: true,
-    language: null
   };
 
   componentWillMount() {
@@ -65,26 +88,25 @@ class MapScreen extends Component {
       <View style={{ flex: 1 }}>
         <View
           style={{
-            flex: 3,
+            flex: 1,
             marginTop: 20,
             borderRadius: 4,
-            borderWidth: 1,
+            borderWidth: 0,
+            flexDirection: 'row',
+            justifyContent: 'center',
             borderColor: "#d6d7da"
+            
           }}
         >
-          <Picker  style={{}} 
-            selectedValue={this.state.language}
-            style={{ height: 50, width: 100 }}
-            onValueChange={(itemValue, itemIndex) =>
-              this.setState({ language: itemValue })
-            }
-          >
-            <Picker.Item label="Java" value="java" />
-            <Picker.Item label="JavaScript" value="js" />
-          </Picker>
+
+          <Menupicker />
+          <Menupicker/>
+          <Menupicker/>
+
         </View>
+
         <MapView
-          style={{ flex: 2 }}
+          style={{ flex: 3 }}
           region={{
             latitude: this.state.location.latitude,
             longitude: this.state.location.longitude,
@@ -94,11 +116,66 @@ class MapScreen extends Component {
         >
           {makerList}
         </MapView>
-        <View style={{ flex: 3, backgroundColor: "blue" }} />
-        <Text> {this.state.language} </Text>
+
+
+        <View style={{ flex: 1, justifyContent: 'center'  }}>
+        <Listfood/>
+        {/* <Text> {this.state.language} </Text> */}
+        </View>
       </View>
     );
   }
 }
+
+
+ class Listfood extends Component {
+
+  render() {
+    const list = [
+      {
+        name: 'City Food',
+        avatar_url: 'https://cdn.pixabay.com/photo/2015/10/02/15/59/olive-oil-968657_960_720.jpg',
+       
+      },
+      {
+        name: 'Halal Eat ',
+        avatar_url: 'https://cdn.pixabay.com/photo/2016/03/10/18/44/top-view-1248955__340.jpg',
+      },
+       {
+        name: 'City Food',
+        avatar_url: 'https://cdn.pixabay.com/photo/2015/10/02/15/59/olive-oil-968657_960_720.jpg',
+       
+      },
+      {
+        name: 'City Food',
+        avatar_url: 'https://cdn.pixabay.com/photo/2015/10/02/15/59/olive-oil-968657_960_720.jpg',
+       
+      },
+      {
+        name: 'City Food',
+        avatar_url: 'https://cdn.pixabay.com/photo/2015/10/02/15/59/olive-oil-968657_960_720.jpg',
+       
+      },
+    ]
+    return (
+      <View >
+         {
+    list.map((l, i) => (
+      <ListItem 
+        key={i}
+        leftAvatar={{ source: { uri: l.avatar_url } }}
+        title={l.name}
+      />
+    ))
+  }
+      </View>
+    )
+  }
+}
+
+
+
+
+
 
 export default MapScreen;
